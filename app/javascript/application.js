d8be514bb11bcc11f89709218c84c07cba63ff31
rabiayilmazqwe@gmail.com <rabiayilmazqwe@gmail.com>
2024-05-29 17:58:12 +0300

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import '@hotwired/stimulus'
+import "stylesheets/application"
+<script type="module" src="javascript/stimulus.js"></script>
+import "trix"
+import "@rails/actiontext"
